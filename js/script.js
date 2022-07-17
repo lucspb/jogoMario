ed95080c242fa4c4cdb9b87570e2bc8706fc23aa
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-07-17 14:05:57 -0300

    adicionando as logicas e estilos ao jogo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     // add a funcao do jump
@@ -11,6 +12,36 @@ const jump = () => {
 
      }, 500);
 }
+
+const loop = setInterval( () => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    // o + altera a string para number
+
+    console.log(marioPosition)
+    
+    // caso o mario encoste no pipe
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        
+        //acaba a animacao do pipe
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        //acaba a animacao do mario
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '50px';
+
+        //finalizar o loop
+        clearInterval(loop);
+    }
+
+} , 10)
+
 // pressiona uma tecla e dispara a funcao do jump
 document.addEventListener('keydown', jump);
 
